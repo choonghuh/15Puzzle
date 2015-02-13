8647e5f2c77c989cabf0baf68f5047df321b2778
Choong Huh <choonghuh@gmail.com>
2015-02-13 14:17:55 -0800

    Initial Commit

---
+++ 15puzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  15puzzle
+//
+//  Created by Choong Choong Huh Huh on 2/13/15.
+//  Copyright (c) 2015 Choong Huh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 15puzzle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  15puzzle
+//
+//  Created by Choong Choong Huh Huh on 2/13/15.
+//  Copyright (c) 2015 Choong Huh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
